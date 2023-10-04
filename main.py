43d630f555aac38005dc15392990923ad101b293
Ryan Ramos <ryan.ramosenhs@gmail.com>
2023-10-04 15:47:38 -0500

    file search for beatmap based on replay file done

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import os
+import os, re
 import config
 from tkinter import filedialog as fd
 from Beatmaps import Beatmap
 from Decoders.BeatmapDecoder import BeatmapDecoder
 from ossapi import Ossapi
+from osrparse import Replay
 
 client_id = config.CLIENT_ID
 client_secret = config.CLIENT_SECRET
@@ -17,8 +18,23 @@ def main():
     # bm_decoder = BeatmapDecoder()
     # beatmap = bm_decoder.decode(filename)
     
-    top50 = api.ranking("osu", "performance")
-    print(top50.ranking[0].user.username)
+    replay_filename = fd.askopenfilename(initialdir=os.path.join(OSU_PATH, "Replays"),
+                                         filetypes=[("*", ".osr")])
+    replay = Replay.from_path(replay_filename)
+    
+    bm = api.beatmap(checksum=replay.beatmap_hash)
+    
+    bm_file = None
+    dir_regex = re.compile(f'^{bm.beatmapset_id}')
+    file_regex = re.compile(f'.*\[{bm.version}\].*osu$')
+    for root, dir, files in os.walk(os.path.join(OSU_PATH, "Songs")):
+        for d in dir:
+            if dir_regex.match(d):
+                for root, dir, files in os.walk(os.path.join(OSU_PATH, "Songs", d)):
+                    for file in files:
+                        if file_regex.match(file):
+                            print(file)
+    
     return
 
 if __name__ == "__main__":
