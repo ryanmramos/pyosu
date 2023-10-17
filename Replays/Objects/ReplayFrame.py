6e28f5e2468d410b99ca382d47bdc72c2c83fc29
Ryan Ramos <ryan.ramosenhs@gmail.com>
2023-10-17 10:57:18 -0500

    refactored creation of beatmap object and replay_frame list

--- Replays/Objects/ReplayFrame.py
+++ Replays/Objects/ReplayFrame.py
@@ -11,3 +11,14 @@ class ReplayFrame:
 
     def __str__(self):
         return f'<x:{self.X}\t, y:{self.Y}>\t at time {self.Time}ms\t {self.Keys}'
+
+""""
+Keys: 
+    A key that can be pressed during osu!standard gameplay - mouse 1 and 2, key
+    1 and 2, and smoke.
+    M1    = 1 << 0
+    M2    = 1 << 1
+    K1    = 1 << 2
+    K2    = 1 << 3
+    SMOKE = 1 << 4
+"""
--- main.py
+++ main.py
@@ -23,29 +23,15 @@ def main():
     bm = api.beatmap(checksum=replay.beatmap_hash)
     
     # Find the beatmap locally
-    bm_decoder = BeatmapDecoder()
-    beatmap = None
-    dir_regex = re.compile(f'^{bm.beatmapset_id}')          # find directory that starts with beatmap ID
-    file_regex = re.compile(f'.*\[{bm.version}\].*osu$')    # check for file that his beatmap version
-    for root, dir, files in os.walk(os.path.join(OSU_PATH, "Songs")):
-        for d in dir:
-            if dir_regex.match(d):
-                for root, dir, files in os.walk(os.path.join(OSU_PATH, "Songs", d)):
-                    for file in files:
-                        if file_regex.match(file):
-                            beatmap = bm_decoder.decode(os.path.join(OSU_PATH, "Songs", d, file))
-                            break
+    beatmap = find_local_beatmap(bm)
     
     # Check if beatmap was found locally
     if not beatmap:
         print(f'(-) ERROR: Corresponding file for "{replay_filename[replay_filename.rfind("/") + 1:-1]}" not found locally.', file=sys.stderr)
         exit(-1)
 
-    replay_frames = [0] * len(replay.replay_data)
-    last_time = 0
-    for i, replay_data in enumerate(replay.replay_data):
-        replay_frames[i] = ReplayFrame(replay_data.x, replay_data.y, replay_data.time_delta, last_time + replay_data.time_delta, replay_data.keys)
-        last_time = replay_frames[i].Time
+    # Create replay frames to work with (includes explicit frame Time, not just time_delta)
+    replay_frames = create_replay_frames(replay.replay_data)
     
     print(beatmap.HitObjects[0])
     for i, replay_frame in enumerate(replay_frames):
@@ -53,6 +39,34 @@ def main():
             print(replay_frame)
     
     return
+
+def find_local_beatmap(bm_api_response):
+    dir_regex = re.compile(f'^{bm_api_response.beatmapset_id}')          # find directory that starts with beatmap ID
+    file_regex = re.compile(f'.*\[{bm_api_response.version}\].*osu$')    # check for file that his beatmap version
+    for root, dir, files in os.walk(os.path.join(OSU_PATH, "Songs")):
+        for d in dir:
+            if dir_regex.match(d):
+                for root, dir, files in os.walk(os.path.join(OSU_PATH, "Songs", d)):
+                    for file in files:
+                        if file_regex.match(file):
+                            # Local beatmap found, return beatmap object created using beatmap decoder
+                            return BeatmapDecoder().decode(os.path.join(OSU_PATH, "Songs", d, file))
+    
+    # If function reaches here, local beatmap was not found
+    return None
+
+def create_replay_frames(replay_data_list):
+    rf = [0] * len(replay_data_list)
+    last_time = 0
+    for i, replay_data in enumerate(replay_data_list):
+        rf[i] = ReplayFrame(replay_data.x, replay_data.y,
+                            replay_data.time_delta,
+                            last_time + replay_data.time_delta,
+                            replay_data.keys)
+        
+        last_time = rf[i].Time
+    
+    return rf
 
 if __name__ == "__main__":
     main()
